8d6be1d42f3264251725eb77496bde39e47ccfdc
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-02-20 15:25:52 +0530

    fix: register edd payment gateways

--- admin/filters.php
+++ admin/filters.php
@@ -9,3 +9,37 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+/**
+ * This function will register gateways to EDD.
+ *
+ * @param array $gateways List of payment gateways.
+ *
+ * @since 1.0.0
+ *
+ * @return array
+ */
+function stripeforedd_register_gateway( $gateways ) {
+
+	// Stripe Checkout.
+	$gateways['stripe_checkout'] = array(
+		'admin_label'    => __( 'Stripe Checkout', 'stripe-for-edd' ),
+		'checkout_label' => __( 'Checkout', 'stripe-for-edd' ),
+		'supports'       => array(
+			'buy_now'
+		)
+	);
+
+	// Stripe - On Page Credit Card fields.
+	$gateways['stripe'] = array(
+		'admin_label'    => __( 'Stripe', 'stripe-for-edd' ),
+		'checkout_label' => __( 'Credit Card', 'stripe-for-edd' ),
+		'supports'       => array(
+			'buy_now'
+		)
+	);
+
+	return $gateways;
+}
+
+add_filter( 'edd_payment_gateways', 'stripeforedd_register_gateway' );
